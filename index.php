29db898f667fb49117c24a00c376806ab65d5e86
ISHFAK AHMED <ISHFAK.AHMED@cbc.ca>
2017-03-01 13:49:23 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,107 @@
+<?php
+session_start();
+
+if(isset($_SESSION['token'])) {
+   header("Location: main.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+   
+   <head>
+      <title>CBC Live Boss</title>
+      <link rel = "stylesheet" href = "css/bootstrap.min.css">
+      <script src="js/jquery.min.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+      
+      <style>
+         body {
+            padding-top: 40px;
+            padding-bottom: 40px;
+            background-color: #ADABAB;
+         }
+         
+         .form-signin {
+            max-width: 330px;
+            padding: 15px;
+            margin: 0 auto;
+            color: #017572;
+         }
+         
+         .form-signin .form-signin-heading,
+         .form-signin .checkbox {
+            margin-bottom: 10px;
+         }
+         
+         .form-signin .checkbox {
+            font-weight: normal;
+         }
+         
+         .form-signin .form-control {
+            position: relative;
+            height: auto;
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            box-sizing: border-box;
+            padding: 10px;
+            font-size: 16px;
+         }
+         
+         .form-signin .form-control:focus {
+            z-index: 2;
+         }
+         
+         .form-signin input[type="email"] {
+            margin-bottom: -1px;
+            border-bottom-right-radius: 0;
+            border-bottom-left-radius: 0;
+            border-color:#017572;
+         }
+         
+         .form-signin input[type="password"] {
+            margin-bottom: 10px;
+            border-top-left-radius: 0;
+            border-top-right-radius: 0;
+            border-color:#017572;
+         }
+         
+         h2{
+            text-align: center;
+            color: #017572;
+         }
+      </style>
+      
+   </head>
+
+   <?php
+
+   if(isset($_GET['msg']))
+   {
+      $msg = $_GET['msg'];
+   } else {
+      $msg='';
+   }
+
+   ?>
+   
+   <body>
+      
+      <h2>Enter Your MPX Username and Password</h2> 
+      <div class = "container form-signin">
+      </div> <!-- /container -->
+      
+      <div class = "container">
+      
+         <form class = "form-signin" role = "form" action = "authenticate.php" method = "post">
+            <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
+            <input type = "text" class = "form-control" name = "username" placeholder = "first.last@cbc.ca" required autofocus></br>
+            <input type = "password" class = "form-control" name = "password" placeholder = "MPX Password" required>
+            <button class = "btn btn-lg btn-primary btn-block" type = "submit" name = "login">Sign in</button>
+         </form>
+         
+      </div> 
+      
+   </body>
+</html>
